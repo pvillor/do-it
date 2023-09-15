ccfb4f2d9a21f79bd7ade49c3bb8b04300337ce1
pvillor <pvillor@gmail.com>
2023-09-15 19:50:24 -0300

    feat: styled input component

--- src/components/Form/input.tsx
+++ src/components/Form/input.tsx
@@ -8,22 +8,46 @@ import {
 	InputGroup,
 } from "@chakra-ui/react";
 
-import { FaExclamation } from "react-icons/fa";
-
 import { useState, useEffect, useCallback, useRef } from "react";
+import { FieldError } from "react-hook-form";
+import { IconType } from "react-icons";
 
-export const Input = () => {
+interface InputProps extends ChakraInputProps {
+	name: string;
+	label?: string;
+	error?: FieldError | null;
+	icon?: IconType;
+}
+
+export const Input = ({
+	name,
+	label,
+	error = null,
+	icon: Icon,
+	...rest
+}: InputProps) => {
 	return (
 		<FormControl>
-			<FormLabel>Label</FormLabel>
+			{!!label && <FormLabel>{label}</FormLabel>}
 
 			<InputGroup flexDirection="column">
-				<InputLeftElement mt="2.5">
-					<FaExclamation />
-				</InputLeftElement>
-				<ChakraInput placeholder="Email" />
+				{Icon && (
+					<InputLeftElement mt="2.5">
+						<Icon />
+					</InputLeftElement>
+				)}
+				<ChakraInput
+					name={name}
+					bg="gray.50"
+					variant="outline"
+					_hover={{ bgColor: "gray.100" }}
+					_placeholder={{ color: "gray.300" }}
+					size="lg"
+					h="60px"
+					{...rest}
+				/>
 
-				<FormErrorMessage>Erro</FormErrorMessage>
+				{!!error && <FormErrorMessage>{error.message}</FormErrorMessage>}
 			</InputGroup>
 		</FormControl>
 	);
